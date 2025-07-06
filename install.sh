b6bc104c6d0983c3fcf372d4b31a22c8bddbed5b
Studio Justin Braun <83368114+studio-justin-braun@users.noreply.github.com>
2025-07-06 13:32:09 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ sudo apt-get update
 sudo apt-get install -y python3 python3-pip python3-tk git bluez pulseaudio
 
 # Install Python packages if any
-pip3 install --upgrade pip
-pip3 install --upgrade pybluez
+pip3 install --break-system-packages --upgrade pip
+pip3 install --break-system-packages --upgrade pybluez
 
 cat <<MSG
 Installation complete. Use 'python3 src/main.py' to start the GUI.
